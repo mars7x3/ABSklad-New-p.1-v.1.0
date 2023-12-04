2c7e217865ca2120f1593eb99da7f2f0687a703b
Runn1dDefault <runniddefault@gmail.com>
2023-12-05 00:58:50 +0600

    added orders, product and category inventories. And added product retrieve

---
+++ crm_manager/mixins.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import IsAuthenticated
+
+from crm_manager.paginations import AppPaginationClass
+from crm_manager.permissions import IsManager
+
+
+class ManagerMixin:
+    permission_classes = (IsAuthenticated, IsManager,)
+    pagination_class = AppPaginationClass
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        staff_profile = self.request.user.staff_profile
+        context.setdefault("city", staff_profile.city)
+        context.setdefault("stock_id", staff_profile.stock.id)
+        return context
--- crm_manager/permissions.py
+++ crm_manager/permissions.py
@@ -3,5 +3,11 @@ from rest_framework.permissions import BasePermission
 
 class IsManager(BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_manager and getattr(request.user, 'staff_profile', None
-                                                                    )
+        return request.user and request.user.is_manager and getattr(request.user, 'staff_profile', None)
+
+
+class OrderPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'PATCH':
+            return obj.status == 'Новый' or (obj.status == 'Оплачено' and obj.type_status == 'Баллы')
+        return False
