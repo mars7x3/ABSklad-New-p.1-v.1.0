7b38e0fc3430fe06e35f91c35d668d82e12fd0bf
mars7x3 <m.ysakov.jcc@gmail.com>
2023-12-07 14:41:27 +0600

    change promo serializer

--- promotion/serializers.py
+++ promotion/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 
 from .models import *
-from product.models import AsiaProduct, ProductImage
+from product.models import AsiaProduct, ProductImage, ProductPrice
 
 
 class StoryListSerializer(serializers.ModelSerializer):
@@ -28,15 +28,26 @@ class StoryProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AsiaProduct
-        fields = ('id', 'description', 'avg_rating', 'reviews_count')
+        fields = ('id', 'description', 'avg_rating', 'reviews_count', 'title')
 
     def to_representation(self, instance):
         user = self.context.get('request').user
         rep = super().to_representation(instance)
-        rep['images'] = StoryProductImageSerializer(instance.images.first(), context=self.context).data
-        rep['price'] = instance.prices.filter(city=user.dealer_profile.price_city,
-                                              d_status=user.dealer_profile.dealer_status).first().price
+        rep['image'] = StoryProductImageSerializer(instance.images.first(), context=self.context).data
+        rep['price'] = StoryProductPriceSerializer(instance.prices.filter(city=user.dealer_profile.price_city,
+                                                   d_status=user.dealer_profile.dealer_status).first(),
+                                                   context=self.context).data
         return rep
+
+
+class StoryProductPriceSerializer(serializers.ModelSerializer):
+    price = serializers.DecimalField(max_digits=100, decimal_places=2, coerce_to_string=False)
+    old_price = serializers.DecimalField(max_digits=100, decimal_places=2, coerce_to_string=False)
+    discount = serializers.DecimalField(max_digits=100, decimal_places=2, coerce_to_string=False)
+
+    class Meta:
+        model = ProductPrice
+        fields = ('price', 'old_price', 'discount', 'discount_status')
 
 
 class StoryProductImageSerializer(serializers.ModelSerializer):
