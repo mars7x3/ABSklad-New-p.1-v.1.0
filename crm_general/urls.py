ce7a27796448e75dda53ba05c7eb95d45cd0dc70
Runn1dDefault <runniddefault@gmail.com>
2023-12-24 23:22:33 +0600

    refactoring and then collect rop urls

--- crm_general/urls.py
+++ crm_general/urls.py
@@ -2,12 +2,41 @@ from django.urls import path, re_path, include
 from rest_framework.routers import SimpleRouter
 
 from .manager.views import (
-    OrderListAPIView, OrderRetrieveAPIView, OrderChangeActivityView, OrderCreateAPIView,
-    DealerListViewSet, DealerBirthdayListAPIView, DealerRetrieveAPIView,
-    DealerBalanceHistoryListAPIView, DealerBasketListAPIView,
-    ProductPriceListAPIView, CollectionListAPIView, CategoryListAPIView, ProductRetrieveAPIView, BalanceViewSet,
-    ReturnListAPIView, ReturnRetrieveAPIView, ReturnUpdateAPIView,
-    BalancePlusManagerView
+    OrderListAPIView as ManagerOrderListView,
+    OrderRetrieveAPIView as ManagerOrderRetrieveView,
+    OrderChangeActivityView as ManagerOrderChangeActivityView,
+    OrderCreateAPIView as ManagerOrderCreateAPIView,
+    DealerListViewSet as ManagerDealerListViewSet,
+    DealerCreateAPIView as ManagerDealerCreateAPIView,
+    DealerBirthdayListAPIView as ManagerDealerBirthdayListAPIView,
+    DealerRetrieveAPIView as ManagerDealerRetrieveAPIView,
+    DealerBalanceHistoryListAPIView as ManagerDealerBalanceHistoryListAPIView,
+    DealerBasketListAPIView as ManagerDealerBasketListAPIView,
+    ProductPriceListAPIView as ManagerProductPriceListAPIView,
+    CollectionListAPIView as ManagerCollectionListAPIView,
+    CategoryListAPIView as ManagerCategoryListAPIView,
+    ProductRetrieveAPIView as ManagerProductRetrieveAPIView,
+    BalanceViewSet as ManagerBalanceViewSet,
+    ReturnListAPIView as ManagerReturnListAPIView,
+    ReturnRetrieveAPIView as ManagerReturnRetrieveAPIView,
+    ReturnUpdateAPIView as ManagerReturnUpdateAPIView,
+    BalancePlusManagerView as ManagerBalancePlusManagerView
+)
+
+from .rop.views import (
+    ManagerListAPIView as RopManagerListAPIView,
+    ManagerRetrieveAPIView as RopManagerRetrieveAPIView,
+    ManagerCreateAPIView as RopManagerCreateAPIView,
+    DealerListViewSet as RopDealerListViewSet,
+    DealerRetrieveAPIView as RopDealerRetrieveAPIView,
+    DealerBalanceHistoryListAPIView as RopDealerBalanceHistoryListAPIView,
+    DealerBasketListAPIView as RopDealerBasketListAPIView,
+    DealerStatusListAPIView as RopDealerStatusListAPIView,
+    DealerStatusCreateAPIView as RopDealerStatusCreateAPIView,
+    CollectionListAPIView as RopCollectionListAPIView,
+    CategoryListAPIView as RopCategoryListAPIView,
+    ProductPriceListAPIView as RopProductPriceListAPIView,
+    BalanceViewSet as BalanceViewSet
 )
 
 from .director.views import *
@@ -19,7 +48,6 @@ director_router.register("director/product/detail", DirectorProductCRUDView)
 director_router.register("director/discount/crud", DirectorDiscountCRUDView)
 director_router.register("director/dealer/list", DirectorDealerListView)
 director_router.register("director/dealer/crud", DirectorDealerCRUDView)
-
 
 director_urlpatterns = [
     path("director/collection/list/", DirectorCollectionListView.as_view()),
@@ -34,62 +62,87 @@ director_urlpatterns = [
     path("director/discount/city/list/", DirectorDiscountCityView.as_view()),
     path("director/discount/product/list/", DirectorDiscountAsiaProductView.as_view()),
 
-
     path("", include(director_router.urls)),
 ]
 
 # --------------------------- MANAGER
 manager_router = SimpleRouter()
-manager_router.register("dealers", DealerListViewSet, basename="crm_general-manager-dealers")
-manager_router.register("balances", BalanceViewSet, basename="crm_general-manager-balances")
+manager_router.register("dealers", ManagerDealerListViewSet, basename="crm_general-manager-dealers")
+manager_router.register("balances", ManagerBalanceViewSet, basename="crm_general-manager-balances")
 
 manager_urlpatterns = [
     # Dealers
-    path("manager/dealers/birthdays/", DealerBirthdayListAPIView.as_view(),
+    path("manager/dealers/create/", ManagerDealerCreateAPIView.as_view(), name="crm_general-manager-dealers-create"),
+    path("manager/dealers/birthdays/", ManagerDealerBirthdayListAPIView.as_view(),
          name="crm_general-manager-dealers-birthdays-list"),
-    re_path("^manager/dealers/(?P<user_id>.+)/detail/$", DealerRetrieveAPIView.as_view(),
+    re_path("^manager/dealers/(?P<user_id>.+)/detail/$", ManagerDealerRetrieveAPIView.as_view(),
             name="crm_general-manager-dealers-detail"),
-    re_path("^manager/dealers/(?P<user_id>.+)/balance-history/$", DealerBalanceHistoryListAPIView.as_view(),
+    re_path("^manager/dealers/(?P<user_id>.+)/balance-history/$", ManagerDealerBalanceHistoryListAPIView.as_view(),
             name="crm_general-manager-dealers-balance-history-list"),
-    re_path("^manager/dealers/(?P<user_id>.+)/basket-history/$", DealerBasketListAPIView.as_view(),
+    re_path("^manager/dealers/(?P<user_id>.+)/basket-history/$", ManagerDealerBasketListAPIView.as_view(),
             name="crm_general-manager-dealers-basket-history-list"),
     # Orders
-    path("manager/orders/", OrderListAPIView.as_view(), name="crm_general-manager-orders-list"),
-    path("manager/orders/create/", OrderCreateAPIView.as_view(), name="crm_general-manager-orders-create"),
-    re_path("^manager/orders/(?P<order_id>.+)/change-activity/$", OrderChangeActivityView.as_view(),
+    path("manager/orders/", ManagerOrderListView.as_view(), name="crm_general-manager-orders-list"),
+    path("manager/orders/create/", ManagerOrderCreateAPIView.as_view(), name="crm_general-manager-orders-create"),
+    re_path("^manager/orders/(?P<order_id>.+)/change-activity/$", ManagerOrderChangeActivityView.as_view(),
             name="crm_general-manager-orders-update-activity"),
-    re_path("^manager/orders/(?P<order_id>.+)/detail/$", OrderRetrieveAPIView.as_view(),
+    re_path("^manager/orders/(?P<order_id>.+)/detail/$", ManagerOrderRetrieveView.as_view(),
             name="crm_general-manager-orders-detail"),
     # Products
-    path("manager/collections/", CollectionListAPIView.as_view(), name="crm_general-manager-collections-list"),
-    path("manager/categories/", CategoryListAPIView.as_view(), name="crm_general-manager-categories-list"),
-    path("manager/products/", ProductPriceListAPIView.as_view(), name="crm_general-manager-products-list"),
-    re_path("^manager/products/(?P<product_id>.+)/detail/$", ProductRetrieveAPIView.as_view(),
+    path("manager/collections/", ManagerCollectionListAPIView.as_view(), name="crm_general-manager-collections-list"),
+    path("manager/categories/", ManagerCategoryListAPIView.as_view(), name="crm_general-manager-categories-list"),
+    path("manager/products/", ManagerProductPriceListAPIView.as_view(), name="crm_general-manager-products-list"),
+    re_path("^manager/products/(?P<product_id>.+)/detail/$", ManagerProductRetrieveAPIView.as_view(),
             name="crm_general-manager-product-detail"),
     # Returns
-    path("manager/returns/", ReturnListAPIView.as_view(), name="crm_general-manager-returns-list"),
-    re_path("^manager/returns/(?P<return_id>.+)/detail/$", ReturnRetrieveAPIView.as_view(),
+    path("manager/returns/", ManagerReturnListAPIView.as_view(), name="crm_general-manager-returns-list"),
+    re_path("^manager/returns/(?P<return_id>.+)/detail/$", ManagerReturnRetrieveAPIView.as_view(),
             name="crm_general-manager-returns-detail"),
-    re_path("^manager/returns/(?P<return_id>.+)/update/$", ReturnUpdateAPIView.as_view(),
+    re_path("^manager/returns/(?P<return_id>.+)/update/$", ManagerReturnUpdateAPIView.as_view(),
             name="crm_general-manager-returns-update"),
     # Balances and Other
-    path("manager/balance/plus/", BalancePlusManagerView.as_view(), name="crm_general-manager-balance-plus-create"),
+    path("manager/balance/plus/", ManagerBalancePlusManagerView.as_view(),
+         name="crm_general-manager-balance-plus-create"),
     path("manager/", include(manager_router.urls)),
 ]
 
 # --------------------------- ROP
 rop_router = SimpleRouter()
-# rop_router.register("managers", ManagerRopViewSet)
-
+rop_router.register("dealers", RopDealerListViewSet, basename="crm_general-rop-dealers")
+rop_router.register("balances", BalanceViewSet, basename="crm_general-rop-balances")
 
 rop_urlpatterns = [
+    # Managers
+    path("rop/managers/", RopManagerListAPIView.as_view(), name="crm_general-rop-managers-list"),
+    re_path(r"^rop/managers/(?P<user_id>.+)/create/$", RopManagerCreateAPIView.as_view(),
+            name="crm_general-rop-managers-create"),
+    re_path(r"^rop/managers/(?P<user_id>.+)/detail/$", RopManagerRetrieveAPIView.as_view(),
+            name="crm_general-rop-managers-detail"),
+
+    # Dealers
+    re_path("^rop/dealers/(?P<user_id>.+)/detail/$", RopDealerRetrieveAPIView.as_view(),
+            name="crm_general-rop-dealers-detail"),
+    re_path("^rop/dealers/(?P<user_id>.+)/balance-history/$", RopDealerBalanceHistoryListAPIView.as_view(),
+            name="crm_general-rop-dealers-balance-history-list"),
+    re_path("^rop/dealers/(?P<user_id>.+)/basket-history/$", RopDealerBasketListAPIView.as_view(),
+            name="crm_general-rop-dealers-basket-history-list"),
+    path("rop/dealer-statuses/", RopDealerStatusListAPIView.as_view(), name="crm_general-rop-dealer-status-list"),
+    path("rop/dealer-status/create/", RopDealerStatusCreateAPIView.as_view(),
+         name="crm_general-rop-dealer-status-create"),
+
+    # Products
+    path("rop/collections/", RopCollectionListAPIView.as_view(), name="crm_general-rop-collections-list"),
+    path("rop/categories/", RopCategoryListAPIView.as_view(), name="crm_general-rop-categories-list"),
+    path("rop/products/", RopProductPriceListAPIView.as_view(), name="crm_general-rop-products-list"),
+    re_path("^manager/products/(?P<product_id>.+)/detail/$", ManagerProductRetrieveAPIView.as_view(),
+            name="crm_general-rop-product-detail"),
+
     path("rop/", include(rop_router.urls)),
 ]
-
+# ---------------------------
 
 crm_router = SimpleRouter()
 crm_router.register("crm/collection/crud", CollectionCRUDView)
-
 
 crm_urlpatterns = [
     path("crm/product/images/create/", ProductImagesCreate.as_view()),
@@ -97,10 +150,8 @@ crm_urlpatterns = [
     path("crm/stock/list/", StockListView.as_view()),
     path("crm/user/image/cd", UserImageCDView.as_view()),
 
-
     path("", include(crm_router.urls)),
 ]
-
 
 # + some_urlpatterns
 urlpatterns = manager_urlpatterns + rop_urlpatterns + director_urlpatterns + crm_urlpatterns
