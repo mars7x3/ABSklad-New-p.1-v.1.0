8242153fe671de4bb95eff9fb6007464b2753fc1
mars7x3 <m.ysakov.jcc@gmail.com>
2023-12-24 17:01:02 +0600

    add translit collection

--- product/models.py
+++ product/models.py
@@ -6,7 +6,7 @@ from general_service.models import City, Stock
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, blank=True, null=True)
     image = models.FileField(upload_to='category', blank=True, null=True)
     uid = models.CharField(max_length=50, default='00000000-0000-0000-0000-000000000000')
     is_active = models.BooleanField(default=False)
@@ -23,7 +23,7 @@ class Category(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200, unique=True)
+    slug = models.CharField(max_length=200, unique=True, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
