3cea64e3f1ee7824d85a06edc280fb1f055df0bf
mars7x3 <m.ysakov.jcc@gmail.com>
2023-12-28 06:24:29 +0600

    add accountant urls

--- crm_general/accountant/serializers.py
+++ crm_general/accountant/serializers.py
@@ -4,7 +4,7 @@ from django.db import transaction
 from django.utils import timezone
 from rest_framework import serializers
 
-from account.models import MyUser, DealerProfile
+from account.models import MyUser, DealerProfile, BalanceHistory, BalancePlus
 from order.models import MyOrder, OrderReceipt, OrderProduct
 from product.models import AsiaProduct
 
@@ -30,7 +30,6 @@ class MyOrderDealerSerializer(serializers.ModelSerializer):
         rep = super().to_representation(instance)
         rep['name'] = instance.user.name
         rep['city_title'] = instance.city.title if instance.city else None
-
         return rep
 
 
@@ -73,4 +72,58 @@ class OrderAsiaProductSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep['category_title'] = instance.category.title if instance.category else None
+        return rep
+
+
+class DealerProfileListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DealerProfile
+        fields = ('city',)
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['dealer_status'] = instance.dealer_status.title if instance.dealer_status else None
+        rep['city'] = instance.city.title if instance.city else None
+        rep['user_info'] = MyUserSerializer(instance.user, context=self.context).data
+        rep['amount_crm'] = instance.wallet.amount_crm
+        rep['amount_1c'] = instance.wallet.amount_1c
+        rep['amount_paid'] = sum(instance.orders.filter(is_active=True, status__in=['paid', 'wait']).values_list('price'))
+        last_trans = instance.user.money_docs.filter(is_active=True).last()
+        rep['last_trans'] = last_trans.created_at if last_trans else None
+        return rep
+
+
+class MyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = ('name', 'phone', 'email')
+
+
+class DirBalanceHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BalanceHistory
+        fields = '__all__'
+
+
+class BalancePlusListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BalancePlus
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['city'] = instance.city.title if instance.city else None
+        rep['user_info'] = BalancePlusDealerSerializer(instance.dealer, context=self.context).data
+        return rep
+
+
+class BalancePlusDealerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DealerProfile
+        fields = ('city', )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['user_info'] = MyUserSerializer(instance.user, context=self.context).data
+        rep['city_title'] = instance.city.title if instance.city else None
         return rep
--- crm_general/accountant/views.py
+++ crm_general/accountant/views.py
@@ -10,15 +10,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
+from account.models import DealerProfile, MyUser, Wallet, BalancePlus
 from crm_general.accountant.permissions import IsAccountant
-from crm_general.accountant.serializers import MyOrderListSerializer, MyOrderDetailSerializer
+from crm_general.accountant.serializers import MyOrderListSerializer, MyOrderDetailSerializer, \
+    DealerProfileListSerializer, DirBalanceHistorySerializer, BalancePlusListSerializer
 from crm_general.views import CRMPaginationClass
+from one_c.models import MoneyDoc
 from order.models import MyOrder
 
 
 class AccountantOrderListView(viewsets.ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated, IsAccountant]
-    queryset = MyOrder.objects.all()
+    queryset = MyOrder.objects.filter(is_active=True)
     serializer_class = MyOrderListSerializer
     pagination_class = CRMPaginationClass
 
@@ -48,6 +51,91 @@ class AccountantOrderListView(viewsets.ReadOnlyModelViewSet):
         if type_status:
             kwargs['type_status'] = type_status
 
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+        if start_date and end_date:
+            start_date = timezone.make_aware(datetime.datetime.strptime(start_date, "%d-%m-%Y"))
+            end_date = timezone.make_aware(datetime.datetime.strptime(end_date, "%d-%m-%Y"))
+            end_date = end_date + datetime.timedelta(days=1)
+            kwargs['created_at__gte'] = start_date
+            kwargs['created_at__lte'] = end_date
+
+        queryset = queryset.filter(**kwargs)
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True, context=self.get_renderer_context()).data
+        return self.get_paginated_response(serializer)
+
+
+class AccountantOrderTotalInfoView(APIView):
+    permission_classes = [IsAuthenticated, IsAccountant]
+
+    def get(self, request):
+        orders_kwargs = {'is_active': True}
+        docs_kwargs = {'is_active': True}
+
+        name = request.query_params.get('name')
+        if name:
+            orders_kwargs['author__user__name__icontains'] = name
+            docs_kwargs['user__name__icontains'] = name
+
+        city = request.query_params.get('city')
+        if city:
+            orders_kwargs['author__city__slug'] = city
+            docs_kwargs['user__dealer_profile__city__slug'] = city
+
+        o_status = request.query_params.get('status')
+        if o_status:
+            orders_kwargs['status'] = o_status
+
+        type_status = request.query_params.get('type_status')
+        if type_status:
+            orders_kwargs['type_status'] = type_status
+            if type_status == 'visa':
+                docs_kwargs['status'] = 'Без нал'
+            else:
+                docs_kwargs['status'] = 'Нал'
+
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+        if start_date and end_date:
+            start_date = timezone.make_aware(datetime.datetime.strptime(start_date, "%d-%m-%Y"))
+            end_date = timezone.make_aware(datetime.datetime.strptime(end_date, "%d-%m-%Y"))
+            end_date = end_date + datetime.timedelta(days=1)
+            orders_kwargs['created_at__gte'] = start_date
+            orders_kwargs['created_at__lte'] = end_date
+            docs_kwargs['created_at__gte'] = start_date
+            docs_kwargs['created_at__lte'] = end_date
+        orders = MyOrder.objects.filter(**orders_kwargs)
+        docs = MoneyDoc.objects.filter(**docs_kwargs)
+        sale_sum = sum(orders.values_list('price', flat=True))
+        profit_sum = sum(docs.values_list('amount', flat=True))
+        response = {'sale_sum': sale_sum, 'profit_sum': profit_sum}
+        return Response(response, status=status.HTTP_200_OK)
+
+
+class AccountantBalanceListView(mixins.ListModelMixin, GenericViewSet):
+    permission_classes = [IsAuthenticated, IsAccountant]
+    queryset = DealerProfile.objects.all()
+    serializer_class = DealerProfileListSerializer
+    pagination_class = CRMPaginationClass
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, **kwargs):
+        queryset = self.get_queryset()
+        kwargs = {}
+
+        name = request.query_params.get('name')
+        if name:
+            kwargs['user__name__icontains'] = name
+
+        city = request.query_params.get('city')
+        if city:
+            kwargs['city__slug'] = city
+
+        dealer_status = request.query_params.get('status')
+        if dealer_status:
+            kwargs['dealer_status'] = dealer_status
+
         queryset = queryset.filter(**kwargs)
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(page, many=True, context=self.get_renderer_context()).data
@@ -55,7 +143,61 @@ class AccountantOrderListView(viewsets.ReadOnlyModelViewSet):
         return self.get_paginated_response(serializer)
 
 
+class AccountantBalanceHistoryListView(APIView):
+    """
+    {"start": "14-12-2023",
+    "end": "14-12-2023",
+    "user_id": user_id,
+    """
+    permission_classes = [IsAuthenticated, IsAccountant]
+
+    def get(self, request):
+        user_id = request.query_params.get('user_id')
+        start = request.query_params.get('start')
+        end = request.query_params.get('end')
+        start_date = timezone.make_aware(datetime.datetime.strptime(start, "%d-%m-%Y"))
+        end_date = timezone.make_aware(datetime.datetime.strptime(end, "%d-%m-%Y"))
+        end_date = end_date + datetime.timedelta(days=1)
+
+        user = MyUser.objects.filter(id=user_id).first()
+        balance_histories = user.dealer_profile.balance_histories.filter(created_at__gte=start_date,
+                                                                         created_at__lte=end_date)
+        response_data = DirBalanceHistorySerializer(balance_histories, many=True, context=self.get_renderer_context()).data
+        return Response(response_data, status=status.HTTP_200_OK)
 
 
+class AccountantTotalEcoBalanceView(APIView):
+    """
+    {
+    "user_id": user_id,
+    "start_date": "start_date",
+    "end_date": "end_date"
+    }
+    """
+    permission_classes = [IsAuthenticated, IsAccountant]
 
+    def get(self, request):
+        user_id = request.query_params.get('user_id')
+        kwargs = {'is_active': True, 'author__user_id': user_id, 'status__in': ['success', 'sent', 'paid',
+                                                                                'wait']}
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+
+        if start_date and end_date:
+            start_date = timezone.make_aware(datetime.datetime.strptime(start_date, "%d-%m-%Y"))
+            end_date = timezone.make_aware(datetime.datetime.strptime(end_date, "%d-%m-%Y"))
+            end_date = end_date + datetime.timedelta(days=1)
+            kwargs['created_at__gte'] = start_date
+            kwargs['created_at__lte'] = end_date
+
+        amount_eco = sum(MyOrder.objects.filter(**kwargs).values_list('order_products__discount', flat=True))
+        amount_crm = Wallet.objects.filter(dealer__user_id=user_id).first().amount_crm
+
+        return Response({"amount_eco": amount_eco, "amount_crm": amount_crm}, status=status.HTTP_200_OK)
+
+
+class BalancePlusListView(mixins.ListModelMixin, GenericViewSet):
+    permission_classes = [IsAuthenticated, IsAccountant]
+    queryset = BalancePlus.objects.all()
+    serializer_class = BalancePlusListSerializer
 
