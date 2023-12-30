7f1e9857bb608aeb7a2e040ac968bf3f889cde82
Runn1dDefault <runniddefault@gmail.com>
2023-12-30 16:21:53 +0600

    added daphne logging

--- absklad_commerce/settings.py
+++ absklad_commerce/settings.py
@@ -222,3 +222,14 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     "TOKEN_OBTAIN_SERIALIZER": "account.serializers.UserLoginSerializer",
 }
+
+LOGGING = {
+    "loggers": {
+        'daphne': {
+            'handlers': [
+                'console',
+            ],
+            'level': 'DEBUG'
+        }
+    }
+}
