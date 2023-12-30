ab3a338fa752f07e941cfd406c607b8fb9265c08
Runn1dDefault <runniddefault@gmail.com>
2023-12-31 04:46:34 +0600

    changed chat base url to ws for receiving requests in chat daphne

--- absklad_commerce/urls.py
+++ absklad_commerce/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('api/v1/', include('order.urls')),
     path('api/v1/', include('product.urls')),
     path('api/v1/', include('promotion.urls')),
-    path('api/v1/chat/', include('chat.urls')),
+    path('ws/chat/', include('chat.urls')),
     path('api/v1/', include('crm_general.urls'))
 ]
 
