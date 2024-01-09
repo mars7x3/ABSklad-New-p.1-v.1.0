17d25b717367cd9e1dd96f1102f747ebad110b7a
MAC M1 <m.isakov.jcc@gmail.com>
2024-01-09 13:15:09 +0600

    sync prods 1c

--- one_c/utils.py
+++ one_c/utils.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-from account.models import DealerStatus
+from account.models import DealerStatus, MyUser, Wallet, DealerProfile
 from general_service.models import Stock, City, PriceType
 from product.models import AsiaProduct, Category, ProductCount, ProductPrice, Collection
 
@@ -129,5 +129,71 @@ def synchronization_1C_to_back(request):  # sync product 1C -> CRM
             product.counts.all().delete()
             p_count_data = [ProductCount(stock=s, product=product) for s in Stock.objects.all()]
             ProductCount.objects.bulk_create([ProductCount(**i) for i in p_count_data])
+
+
+def sync_dealer():
+    url = 'http://91.211.251.134/ab1c/hs/asoi/clients'
+    username = 'Директор'
+    password = '757520ля***'
+    response = requests.get(url, auth=(username.encode('utf-8'), password.encode('utf-8')))
+    response_data = json.loads(response.content)
+
+    clients = response_data.get('clients')
+    dealer_status = DealerStatus.objects.filter(title='C')
+    data = []
+    count = 0
+    for c in clients:
+        count += 1
+        print(count)
+        user = MyUser.objects.filter(uid=c.get('UID'))
+        if not user:
+
+            password = 'absklad123'
+            dict_ = {'name': c.get('Name'),
+                     'uid': c.get('UID'),
+                     'phone': c.get('Telephone'),
+                     'address': c.get('Address'),
+                     'liability': c.get('Liability'),
+                     'email': c.get('UID') + "@absklad.com",
+                     'password': password,
+                     'pwd': password,
+                     'status': 'dealer',
+                     'dealer_status': dealer_status,
+                     'username': c.get('UID'),
+                     }
+
+            city = City.objects.filter(user_uid=c.get('CityUID')).first()
+            if city:
+                dict_['city'] = city
+            else:
+                dict_['is_active'] = False
+
+            data.append(dict_)
+
+    dealer_data = []
+    wallet_data = []
+    if data:
+        for d in data:
+            price_type = PriceType.objects.filter(title__icontains=d['city'].title)
+            city = d.pop('city')
+            dealer_status = d.pop('dealer_status')
+            user = MyUser.objects.create_user(**d)
+            dealer_data.append(
+                DealerProfile(
+                    user=user,
+                    city=city,
+                    dealer_status=dealer_status,
+                    price_type=price_type
+
+                )
+            )
+            wallet_data.append(user)
+
+    DealerProfile.objects.bulk_create(dealer_data)
+
+    wallet_r = []
+    for deal in wallet_data:
+        wallet_r.append(Wallet(dealer=deal.dealer_profile))
+    Wallet.objects.bulk_create(wallet_r)
 
 
