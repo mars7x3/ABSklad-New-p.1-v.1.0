3a4ddf9163aa04ed8df635ac2fef6f2cc24ddfac
MAC M1 <m.isakov.jcc@gmail.com>
2024-01-09 18:40:42 +0600

    sync 1c product crud

--- one_c/views.py
+++ one_c/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from one_c.utils import sync_prod_crud_1c_crm
+
+
+class SyncProductCRUDVIew(APIView):
+    def post(self, request):
+        sync_prod_crud_1c_crm(request)
+        return Response('Success!', status=status.HTTP_200_OK)
+
