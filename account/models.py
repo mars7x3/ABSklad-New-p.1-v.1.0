79fe39485046c4be5a5676c989fefc4a9fda6dec
beknazarkydykbekov <kydykbekovbeknazar@gmail.com>
2024-01-09 13:03:52 +0600

    changed crm notification (deleted users and cities fields)

--- account/models.py
+++ account/models.py
@@ -215,9 +215,7 @@ class CRMNotification(models.Model):
         ('motivation', 'Мотивация'),
     )
 
-    users = models.ManyToManyField(MyUser, related_name='crm_notifications', blank=True)
-    cities = models.ManyToManyField(City, related_name='crm_notifications', blank=True)
-    groups = models.ManyToManyField(DealerStatus, related_name='crm_notifications', blank=True)
+    dealer_profiles = models.ManyToManyField(DealerProfile, related_name='crm_notifications', blank=True)
     image = models.FileField(upload_to='notification', blank=True, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
