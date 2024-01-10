5f6c16fa88975cfe96154b07a94a76359da7e7b9
beknazarkydykbekov <kydykbekovbeknazar@gmail.com>
2024-01-10 17:33:44 +0600

    added task to set is_active field of banner if banner running out of time

--- crm_general/marketer/tasks.py
+++ crm_general/marketer/tasks.py
@@ -2,16 +2,27 @@ from absklad_commerce.celery import app
 from account.models import Notification
 from django.utils import timezone
 
+from promotion.models import Banner
+
 
 @app.task()
 def create_notifications(notifications: list, image_url, dispatch_date):
     """
     Create every notification for user
     """
-    print('worked')
     if dispatch_date <= timezone.now():
         notification_to_create = [Notification(**n) for n in notifications]
         nots = Notification.objects.bulk_create(notification_to_create)
         for notification in nots:
             notification.image = image_url
             notification.save()
+
+
+@app.task()
+def set_banner_false():
+    current_date = timezone.now()
+    banners = Banner.objects.filter(is_active=True)
+    for banner in banners:
+        if banner.end_time < current_date:
+            banner.is_active = False
+            banner.save()
