eea65b2ffbc4170233929892357087876894a29a
Runn1dDefault <runniddefault@gmail.com>
2024-01-10 19:19:51 +0600

    changes queries of dealer list and dealer relations to filtering by managers field of dealer profile

--- crm_general/rop/mixins.py
+++ crm_general/rop/mixins.py
@@ -40,7 +40,7 @@ class BaseDealerMixin:
         return self.request.user.rop_profile
 
     def get_queryset(self):
-        return super().get_queryset().filter(city__in=self.rop_profile.cities.all())
+        return super().get_queryset().filter(managers__city__in=self.rop_profile.cities.all())
 
 
 class BaseDealerRelationViewMixin:
@@ -53,7 +53,7 @@ class BaseDealerRelationViewMixin:
         return self.request.user.rop_profile
 
     def get_dealers_queryset(self):
-        return DealerProfile.objects.filter(user__status="dealer", city__in=self.rop_profile.cities.all())
+        return DealerProfile.objects.filter(user__status="dealer", managers__city__in=self.rop_profile.cities.all())
 
     def get_dealer_profile(self) -> DealerProfile:
         queryset = self.get_dealers_queryset()
