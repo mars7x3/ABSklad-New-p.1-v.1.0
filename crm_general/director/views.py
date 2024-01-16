085718ce4ccc68f30fdd6b7456666417070030f8
MAC M1 <m.isakov.jcc@gmail.com>
2024-01-16 17:36:12 +0600

    max

--- crm_general/director/views.py
+++ crm_general/director/views.py
@@ -993,7 +993,7 @@ class MaxatTestView(APIView):
             data.append(
                 {
                     'money_id': i.id,
-                    'name': i.user.name,
+                    'name': i.user.name if i.user else 'Нет имени',
                     'created_at': i.created_at,
                     'amount': i.amount
                 }
