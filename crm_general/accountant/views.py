2239766d0eb8323b514309903ecbe8838e53010c
beknazarkydykbekov <kydykbekovbeknazar@gmail.com>
2024-01-22 00:25:10 +0600

    working on pagination bug part 5

--- crm_general/accountant/views.py
+++ crm_general/accountant/views.py
@@ -37,7 +37,7 @@ class AccountantOrderListView(viewsets.ReadOnlyModelViewSet):
     # permission_classes = [IsAuthenticated, IsAccountant]
     queryset = MyOrder.objects.filter(is_active=True)
     serializer_class = MyOrderListSerializer
-    pagination_class = GeneralPurposePagination
+    pagination_class = CRMPaginationClass
 
     def get_serializer_class(self):
         if self.detail:
@@ -75,10 +75,10 @@ class AccountantOrderListView(viewsets.ReadOnlyModelViewSet):
             kwargs['created_at__lte'] = end_date
 
         queryset = queryset.filter(**kwargs)
-        paginator = GeneralPurposePagination()
+        paginator = CRMPaginationClass()
         page = paginator.paginate_queryset(queryset, request)
-        serializer = self.get_serializer(page, many=True, context=self.get_renderer_context()).data
-        return self.get_paginated_response(serializer)
+        serializer = MyOrderListSerializer(page, many=True, context=self.get_renderer_context()).data
+        return paginator.get_paginated_response(serializer)
         # serializer = self.get_serializer(queryset, many=True, context=self.get_renderer_context()).data
         # return Response(serializer, status=status.HTTP_200_OK)
 
