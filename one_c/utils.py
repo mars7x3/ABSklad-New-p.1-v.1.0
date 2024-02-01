84ac07e775208e17711446944fc6a6da59319fb2
MAC M1 <m.isakov.jcc@gmail.com>
2024-02-01 14:04:15 +0600

    fix

--- one_c/utils.py
+++ one_c/utils.py
@@ -749,7 +749,6 @@ def sync_1c_inventory_crud(data):
             data.get('created_at'), "%d.%m.%Y %H:%M:%S") - datetime.timedelta(hours=6),
         'updated_at': datetime.datetime.strptime(
             data.get('created_at'), "%d.%m.%Y %H:%M:%S") - datetime.timedelta(hours=6),
-        'stock': stock,
         'sender': sender,
         'status': 'moderated',
          }
