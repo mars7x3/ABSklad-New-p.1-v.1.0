bbc3e7322217e182055bc9353d574e12bd57b24d
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2024-07-06 14:33:09 +0600

    fix  json not serializable on set form data

--- one_c/cache_utils.py
+++ one_c/cache_utils.py
@@ -1,11 +1,10 @@
 import json
 import logging
-from decimal import Decimal
 from typing import Literal
-from uuid import uuid4
 
 from django.conf import settings
 from django.core.cache import caches
+from django.core.serializers.json import DjangoJSONEncoder
 from django.utils import timezone
 from django.utils.dateformat import DateFormat
 
@@ -30,22 +29,12 @@ def rebuild_cache_key(key: str):
     }
 
 
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, Decimal):
-            # wanted a simple yield str(o) in the next line,
-            # but that would mean a yield on the line with super(...),
-            # which wouldn't work (see my comment below), so...
-            return (str(o) for o in [o])
-        return super().default(o)
-
-
 def set_form_data(user_id: int, data: dict, view_name: str, action: Literal['create', 'update', 'delete']) -> str:
     cache = caches[settings.ONE_C_TASK_CACHE]
     cache_key = build_cache_key(user_id, view_name, action)
 
     try:
-        data = json.dumps(data, cls=DecimalEncoder)
+        data = json.dumps(data, cls=DjangoJSONEncoder)
     except Exception as e:
         logging.error(f"JSON dumps error on body: {data}")
         raise e
