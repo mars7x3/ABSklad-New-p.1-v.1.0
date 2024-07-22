5fc0512c9deabc275994ad31d0b413d83123ff19
mars7x3 <m.ysakov.jcc@gmail.com>
2024-07-23 03:38:39 +0600

    fix

--- crm_general/accountant/permissions.py
+++ crm_general/accountant/permissions.py
@@ -3,6 +3,6 @@ from rest_framework.permissions import BasePermission
 
 class IsAccountant(BasePermission):
     def has_permission(self, request, view):
-        if request.user.status == 'accountant':
+        if request.user.status in ['accountant', 'director']:
             return True
         return False
